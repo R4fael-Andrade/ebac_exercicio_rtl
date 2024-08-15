ba9f0a01ff73f0f6ff97e796a318d55e17a5bb90
Rafael <rafael.sousadrd@gmail.com>
2024-08-15 19:16:28 -0300

    criando teste para inserção de comentários

--- src/components/PostComments/PostComments.test.tsx
+++ src/components/PostComments/PostComments.test.tsx
@@ -1,10 +1,28 @@
 import { fireEvent, render, screen } from '@testing-library/react';
-import Post from '.';
 import PostComment from '.';
+
 
 describe('Teste para o componente PostComment', () => {
     it('Deve renderizar o componente corretamente', () => {
         render(<PostComment/>);
         expect(screen.getByText('Comentar')).toBeInTheDocument();
     });
+    test('Teste para insercão de 2 comentários', () => {
+        const {debug} = render(<PostComment />);
+        
+            //selecionando elementos
+            const textArea = screen.getByRole('textbox');
+            const botao = screen.getByTestId('btn-adicionar');
+            //Simulando a inserção do comentário
+            fireEvent.change(textArea, {target: {value: 'Comentário 1'}})
+            fireEvent.click(botao);
+
+            fireEvent.change(textArea, {target: {value: 'Comentário 2'}})
+            fireEvent.click(botao);
+            debug()
+            //Verificando a quantidade de comentários
+            expect(screen.getAllByRole('listitem').length).toBe(2);
+        
+        
+    })
 });
